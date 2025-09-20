76e1323ad2556438fc39a8e2c5af15adb6513e39
juniorXX1 <141535967+juniorXX1@users.noreply.github.com>
2025-09-21 01:24:28 +0530

    lecture project 09 -21

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SignUpScreen from "./src/screens/SignUpScreen";
+
+import SettingScreen from "./src/screens/SettingScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import "./global.css";
+import SplashScreen from "./src/screens/SplashScreen";
+import SignInScreen from "./src/screens/SignInScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import { ThemeProvider } from "./src/theme/ThemeProvider";
+
+export type RootStack = {
+  SplashScreen: undefined;
+  SignInScreen: undefined;
+  SignUpScreen: undefined;
+  HomeScreen: undefined;
+  SettingScreen: undefined;
+  ProfileScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStack>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+       initialRouteName="SplashScreen"
+       screenOptions={{
+        animation:"fade",
+       }}
+      >
+        
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{
+             headerShown: false
+
+             }}
+        />
+
+        <Stack.Screen
+          name="SignUpScreen"
+          component={SignUpScreen}
+           options={{
+             headerShown: false
+
+             }}
+          
+        />
+
+        <Stack.Screen name="SignInScreen" component={SignInScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,118 @@
+import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
+
+import "../../global.css";
+import CircleShape from "../components/CircleShape";
+import { useEffect, useRef } from "react";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStack } from "../../App";
+import { runOnJS } from "react-native-worklets";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "../theme/ThemeProvider";
+
+type Props = NativeStackNavigationProp<RootStack, "SplashScreen">;
+
+export default function SplashScreen() {
+  const navigation = useNavigation<Props>();
+
+  const opacity = useSharedValue(0);
+
+  useEffect(() => {
+    opacity.value = withTiming(1, { duration: 3000 });
+    const timer = setTimeout(() => {
+      navigation.navigate("SignUpScreen");
+    }, 3000);
+
+    return()=>{
+      clearTimeout(timer);
+    };
+  }, [navigation,opacity]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  const {applied}=useTheme();
+
+  const logo= applied === "dark"?require("../../assets/lightLogo.png") :require("../../assets/logo.png");
+
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center bg-slate-50 dark:bg-slate-950">
+      <StatusBar hidden />
+
+      <CircleShape
+        width={200}
+        height={200}
+        className="bg-slate-900"
+        borderRadius={999}
+        topValue={-25}
+        leftValue={-50}
+      />
+      <CircleShape
+        width={150}
+        height={150}
+        className="bg-sky-950"
+        borderRadius={999}
+        topValue={-30}
+        leftValue={60}
+      />
+
+      <Animated.View style={animatedStyle}>
+        <Image
+          source={logo}
+          style={{ height: 180, width: 220 }}
+        />
+      </Animated.View>
+
+      <Animated.View className="absolute bottom-20" style={animatedStyle}>
+        <View className="justify-center items-center">
+          <Text className="text-xs font-bold text-slate-600 dark:text-slate-200">
+            POWERED BY: {process.env.EXPO_PUBLIC_APP_OWNER}
+          </Text>
+          <Text className="text-xs font-bold text-slate-600 dark:text-slate-200">
+            VERSION: {process.env.EXPO_PUBLIC_APP_VERSION}
+          </Text>
+        </View>
+      </Animated.View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottomContainer: {
+    position: "absolute",
+    bottom: 50,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  companyName: {
+    color: "#6B7280",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  appVersion: {
+    color: "#6B7280",
+    fontWeight: "bold",
+    fontSize: 10,
+  },
+  logoContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 220,
+    height: 180,
+  },
+});
